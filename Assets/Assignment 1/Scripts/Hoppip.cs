9ec79dc59568832c38ebaf71c3eb3a1e7a9918fe
SHERNET\knappse <sebastian.knapp8@gmail.com>
2025-01-30 23:35:08 -0500

    Added background and bushes, added the Zs and made them move
    hoppip now enters other side of screen if they leave the screen

--- Assets/Assignment 1/Scripts/Hoppip.cs
+++ Assets/Assignment 1/Scripts/Hoppip.cs
@@ -9,6 +9,9 @@ public class Hoppip : MonoBehaviour
     public float t;
     float speed;
     Vector2 pos;
+    Vector2 rightEdge;
+    Vector2 leftEdge;
+    public float offsetAmount;
 
     // Start is called before the first frame update
     void Start()
@@ -28,9 +31,24 @@ public class Hoppip : MonoBehaviour
         }
 
         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
-    
-    pos = transform.position;
-        pos.y = Mathf.Lerp(1, 3, floaty.Evaluate(t));
+
+        pos = transform.position;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
+
+        if (screenPos.x + offsetAmount < 0)
+        {
+            pos.x = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width,0)).x;
+           
+        }
+
+        if (screenPos.x - offsetAmount > Screen.width)
+        {
+            pos.x = Camera.main.ScreenToWorldPoint(Vector2.zero).x;
+        }
+
+     
+        pos.y = Mathf.Lerp(2, 4, floaty.Evaluate(t));
         transform.position = pos;
 
 
--- Assets/Assignment 1/Scripts/SleepZs.cs
+++ Assets/Assignment 1/Scripts/SleepZs.cs
@@ -6,8 +6,12 @@ public class SleepZs : MonoBehaviour
 {
 
     public AnimationCurve horiZ;
-    public AnimationCurve VertZ;
+    public AnimationCurve vertZ;
     public float t;
+    public float startH;
+    public float endH;
+    public float startV;
+    public float endV;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,18 @@ public class SleepZs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
+        t += Time.deltaTime * 0.5f;
+
+        if (t > 1)
+        {
+            t = 0;
+        }
         
+        Vector2 pos = transform.position;
+        pos.x = Mathf.Lerp(startH,endH, horiZ.Evaluate(t));
+        pos.y = Mathf.Lerp(startV,endV, vertZ.Evaluate(t));
+
+        transform.position = pos;
+
     }
 }
